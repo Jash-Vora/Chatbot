94eb15dad0230f388a746d2bc54ca437299ddb10
Jash-Vora <133149276+Jash-Vora@users.noreply.github.com>
2024-04-14 12:30:15 +0530

    Update openai.js

--- src/config/openai.js
+++ src/config/openai.js
@@ -1,7 +1,6 @@
 import OpenAI from 'openai';
 const openai = new OpenAI({
-  apiKey: process.env['OPEN_AI_API_KEY'],
-  dangerouslyAllowBrowser: true,
+  apiKey: process.env['OPEN_AI_API_KEY']
 });
 
 export default openai;
